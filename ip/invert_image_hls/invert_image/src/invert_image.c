eb52ece7fe1b573e3c33f44df08fd2d76b63b24a
Frederik Falk Nyboe <ffn@mmmi.sdu.dk>
2022-01-21 12:52:29 +0100

    First commit of IP HLS project

---
+++ ip/invert_image_hls/invert_image/src/invert_image.c
@@ -0,0 +1,17 @@
+#include "invert_image.h"
+
+void invert_image(uint8_t image_in[SIZE], uint8_t image_out[SIZE])
+{
+#pragma HLS TOP
+#pragma HLS INTERFACE s_axilite bundle=BUS_A port=image_in
+#pragma HLS INTERFACE s_axilite bundle=BUS_A port=image_out
+#pragma HLS INTERFACE s_axilite bundle=BUS_A port=return
+
+      loop_x: for (int i = 0; i < IMAGE_X; i++)
+      {
+              loop_y: for (int j = 0; j < IMAGE_Y; j++)
+              {
+                      image_out[i*IMAGE_Y+j] = image_in[i*IMAGE_Y+(IMAGE_Y-1-j)];
+              }
+      }
+}
---
+++ ip/invert_image_hls/invert_image/src/invert_image.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+#define IMAGE_X         480
+#define IMAGE_Y         640
+#define SIZE            IMAGE_X*IMAGE_Y
+
+void invert_image(uint8_t image_in[SIZE], uint8_t image_out[SIZE]);
