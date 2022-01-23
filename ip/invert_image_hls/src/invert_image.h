e0f641725b842b751e24a112b2134d75b6e4a641
frnyb <ffn@mmmi.sdu.dk>
2022-01-23 17:40:04 +0100

    Submodule instead of src

--- ip/invert_image_hls/src/invert_image.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-#define IMAGE_X         480
-#define IMAGE_Y         640
-#define SIZE            IMAGE_X*IMAGE_Y
-//#define SIZE			10
-
-void invert_image(uint8_t image_in[SIZE], uint8_t image_out[SIZE]);
